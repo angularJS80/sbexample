6c05639f5ba9d892e753f643e8b164e0686068db
angularJS80 <bigcen@naver.com>
2018-11-04 09:00:46 +0900

    로드테스트 자체

--- src/main/java/com/example/demo/util/LoadTest2.java
+++ src/main/java/com/example/demo/util/LoadTest2.java
@@ -21,8 +21,8 @@ public class LoadTest2 {
 	
 	//static String postloginUrl = "http://52.193.154.247:28080/sbexample/api/authenticateAsync";
 	static String postloginUrl = "http://localhost:38080/sbexample2/api/authenticateAsync";
-	
-	static int loadCnt = 1000;
+	static int startNum = 0;
+	static int loadCnt = 100;
 	static RestTemplate restTemplate = new RestTemplate();
 
 	
@@ -34,36 +34,36 @@ public class LoadTest2 {
 
 
 
-	CyclicBarrier barrier = new CyclicBarrier(101);
+	CyclicBarrier barrier = new CyclicBarrier(loadCnt+1);
 
 
 	StopWatch main = new StopWatch();
 	main.start();
 
-	for (int i=0; i<loadCnt; i++){
+	for (int i=startNum; i<loadCnt+startNum; i++){
 	es.submit(()->{
-	int idx = counter.addAndGet(1);
-
-	barrier.await();
-
-
-	StopWatch sw = new StopWatch();
-	sw.start();
+		int idx = counter.addAndGet(1);
 	
-	String regRequestJson = createRegUserJson(idx);
-	HttpEntity<String> regEntity = new HttpEntity<String>(regRequestJson,headers);
-	ResponseEntity<String> reganswer = restTemplate.postForEntity(postUserRegUrl, regEntity, String.class);
-	System.out.println("reganswer: "+reganswer);
-	/*
-	String logRequestJson = createLoginUserJson(idx);
-	HttpEntity<String> logEntity = new HttpEntity<String>(logRequestJson,headers);
-	ResponseEntity<String> loganswer = restTemplate.postForEntity(postloginUrl, logEntity, String.class);
-	System.out.println(loganswer);
-	*/
-	sw.stop();
+		barrier.await();
 	
-
-	return null;
+	
+		StopWatch sw = new StopWatch();
+		sw.start();
+		
+		String regRequestJson = createRegUserJson(idx);
+		HttpEntity<String> regEntity = new HttpEntity<String>(regRequestJson,headers);
+		ResponseEntity<String> reganswer = restTemplate.postForEntity(postUserRegUrl, regEntity, String.class);
+		System.out.println("reganswer: "+reganswer);
+		/*
+		String logRequestJson = createLoginUserJson(idx);
+		HttpEntity<String> logEntity = new HttpEntity<String>(logRequestJson,headers);
+		ResponseEntity<String> loganswer = restTemplate.postForEntity(postloginUrl, logEntity, String.class);
+		System.out.println(loganswer);
+		*/
+		sw.stop();
+		
+	
+		return null;
 	});
 	}
 
@@ -83,21 +83,21 @@ public class LoadTest2 {
 				"      \"name\": \"user\"\n" + 
 				"    }\n" + 
 				"  ],\n" + 
-				"  \"createdBy\": \"testuser"+i+"\",\n" +
+				"  \"createdBy\": \"testuser"+startNum+i+"\",\n" +
 				"  \"createdDate\": \"2018-11-03T03:33:12.998Z\",\n" + 
-				"  \"email\": \"testuser"+i+"@test.com\",\n" + 
+				"  \"email\": \"testuser"+startNum+i+"@test.com\",\n" + 
 				"  \"id\": 0,\n" + 
 				"  \"lastModifiedBy\": \"testuser\",\n" + 
 				"  \"lastModifiedDate\": \"2018-11-03T03:33:12.998Z\",\n" + 
-				"  \"login\": \"testuser"+i+"\",\n" + 
-				"  \"name\": \"testuser"+i+"\",\n" + 
+				"  \"login\": \"testuser"+startNum+i+"\",\n" + 
+				"  \"name\": \"testuser"+startNum+i+"\",\n" + 
 				"  \"password\": \"testuser\"\n" + 
 				"}";
 		return regUserJson;
 	}
 	
 	private static String createLoginUserJson(int i) {
-		String logingUserJson = "{\"username\":\"testuser"+i+"\",\"password\":\"testuser\"}";
+		String logingUserJson = "{\"username\":\"testuser"+startNum+i+"\",\"password\":\"testuser\"}";
 		return logingUserJson;
 	}
 }
